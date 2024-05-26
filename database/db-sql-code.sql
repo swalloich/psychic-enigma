7843b9d0c0347c736b67e41c6986cedfdf6d7934
swalloich <nels.jac42@gmail.com>
2024-05-26 00:58:56 -0600

    completed assignment 2

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -228,3 +228,13 @@ VALUES   (
     'White',
     5
   );
+
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+UPDATE inventory
+SET
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+WHERE inv_image LIKE '%/images%' or inv_thumbnail LIKE '%/images%';
