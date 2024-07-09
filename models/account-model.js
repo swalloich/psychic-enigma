c84b3aed49c285c92003947006370daeff5b3235
swalloich <nels.jac42@gmail.com>
2024-07-09 16:48:17 -0600

    Added getAccountByEmail, login post route, and /account/ route

--- models/account-model.js
+++ models/account-model.js
@@ -1,19 +1,5 @@
 const pool = require("../database/")
 
-async function getAccountById(account_id) {
-    try {
-        const data = await pool.query(
-            `SELECT * FROM public.account AS a WHERE a.account_id = $1`,
-            [inventory_id]
-        );
-        if (data.rowCount > 1) {
-            throw Error("More than one result was returned!");
-        }
-    } catch (err) {
-        console.error("getAccountById error: " + err);
-    }
-}
-
 async function registerAccount(account_firstname, account_lastname, account_email, account_password, account_type) {
     try {
         const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *";
@@ -23,4 +9,15 @@ async function registerAccount(account_firstname, account_lastname, account_emai
     }
 }
 
-module.exports = {getAccountById, registerAccount};
+async function getAccountByEmail (account_email) {
+    try {
+        const result = await pool.query(
+            'SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_email = $1',
+            [account_email]);
+        return result.rows[0];
+    } catch (err) {
+        return new Error("No matching email found");
+    }
+}
+
+module.exports = { registerAccount, getAccountByEmail };
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,9 +5,16 @@ const accountController = require("../controllers/accountController");
 const regValidate = require("../utilities/account-validation");
 
 // get routes
+router.get("/", utilities.handleErrors(accountController.buildAccountManagement))
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegistration));
 // post routes
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    utilities.handleErrors(accountController.accountLogin)
+);
 router.post(
     "/register",
     regValidate.registrationRules(),
