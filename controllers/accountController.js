2e7732fe2b8147717e11ab2f377e60787b036515
swalloich <nels.jac42@gmail.com>
2024-07-09 11:31:54 -0600

    removed unneeded function

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -49,23 +49,4 @@ accountController.registerAccount = async function (req, res) {
     }
 }
 
-accountController.buildAccountById = async function (req, res, next) {
-    try {
-        const accountId = req.params.accountId;
-        const data = await acctModel.getAccountById(accountId);
-
-        if (data.length == 0) {
-            const error = new Error("No accounts with that ID were found.");
-            error.status = 404;
-            next(error);
-        }
-
-        // const account = await utilities.buildAccountView(data);
-        let nav = await utilities.getNav()
-        // TODO: finish this.
-    } catch (err) {
-        next(err);
-    }
-}
-
 module.exports = accountController
